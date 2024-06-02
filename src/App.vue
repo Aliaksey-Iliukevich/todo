04de0e44ed4d1ed1d366f01bb789c20069ac69c9
Alexey Iliukevich <aleks.ilykevich@gmail.com>
2024-06-02 17:51:07 +0300

    feat: add filters

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
    <app-header/>
 
-   <app-filters/>
+   <app-filters 
+    :activeFilter="activeFilter" 
+    @setFilter="setFilter"/>
 
     <main class="app-main">
-      <app-todo-list :todos="todos" @toggle-todo="toggleTodo" @remove-todo="removeTodo"/>
+      <app-todo-list 
+        :todos="filteredTodos" 
+        @toggle-todo="toggleTodo" 
+        @remove-todo="removeTodo"/>
 
      <app-add-todo @add-todo="addTodo"/>
     </main>
@@ -20,9 +25,11 @@ import AppTodoList from './components/AppTodoList.vue'
 import AppAddTodo from './components/AppAddTodo.vue'
 import AppFooter from './components/AppFooter.vue'
 import { Todo } from './types/Todo';
+import { Filter } from './types/Filter'
 
 interface State {
-  todos: Todo[]
+  todos: Todo[],
+  activeFilter: Filter
 }
 
 export default defineComponent({
@@ -33,6 +40,19 @@ export default defineComponent({
     AppAddTodo,
     AppFooter
   },
+  computed: {
+    filteredTodos(): Todo[] {
+      switch (this.activeFilter) {
+        case 'Active':
+          return this.todos.filter(todo => !todo.completed)
+        case 'Done':
+          return this.todos.filter(todo => todo.completed)
+        case 'All':
+        default:
+        return this.todos
+      }
+    }
+  },
   data() : State {
     return {
       todos: [
@@ -40,7 +60,8 @@ export default defineComponent({
         {id: 1, text: 'Learn the basics of Typescript', completed: false},
         {id: 2, text: 'Subscribe to the channel', completed: false},
         {id: 3, text: 'Learn the basics of HTML', completed: false},
-      ]
+      ],
+      activeFilter: 'All'
     }
   },
   methods: {
@@ -56,6 +77,9 @@ export default defineComponent({
     },
     removeTodo(id: number | undefined) {
       this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    setFilter(filter: Filter) {
+      this.activeFilter = filter
     }
   }
 })
--- src/components/AppFilters.vue
+++ src/components/AppFilters.vue
@@ -1,11 +1,46 @@
 <template>
      <aside class="app-filters">
       <section class="toggle-group">
-        <button class="button button--primary">All</button>
-        <button class="button">Active</button>
-        <button class="button">Done</button>
+        <button
+          class="button" 
+          :class="{'button--primary': activeFilter === filter}"
+          v-for="filter in filters"
+          :key="filter"
+          @click="setFilter(filter)"
+        >
+          {{ filter }}
+        </button>
       </section>
     </aside>
 </template>
 
-<script></script>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Filter } from '../types/Filter'
+
+interface State {
+  filters: Filter[]
+}
+
+export default defineComponent({
+  props: {
+    activeFilter: {
+      type: String as PropType<Filter>,
+      required: true
+    }
+  },
+  emits: {
+    setFilter: (filter: Filter) => filter
+  },
+  data(): State {
+    return{
+      filters: ['All', 'Active', 'Done']
+    }
+  },
+  methods: {
+    setFilter(filter: Filter) {
+      this.$emit('setFilter', filter)
+    }
+  }
+})
+</script>
