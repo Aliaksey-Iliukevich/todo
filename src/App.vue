a700c00f710b91b0005e5f38d3be201696af9c30
Alexey Iliukevich <aleks.ilykevich@gmail.com>
2024-06-02 12:48:35 +0300

    feat: add components

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+   <app-header/>
+
+   <app-filters/>
+
+    <main class="app-main">
+      <app-todo-list/>
+
+     <app-add-todo/>
+    </main>
+
+    <app-footer/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppHeader from './components/AppHeader.vue'
+import AppFilters from './components/AppFilters.vue'
+import AppTodoList from './components/AppTodoList.vue'
+import AppAddTodo from './components/AppAddTodo.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default defineComponent({
+  components: {
+    AppHeader,
+    AppFilters,
+    AppTodoList,
+    AppAddTodo,
+    AppFooter
+  }
+})
+</script>
